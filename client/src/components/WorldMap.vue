14517d89f08dc532fa650a2a12ccabc005fc34e6
shaunhk <shaunhk@hotmail.com>
2019-07-01 15:53:39 +0100

    placeholder zoom:

--- client/src/components/WorldMap.vue
+++ client/src/components/WorldMap.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <l-map style="height: 500px; width: 100%" :options="mapOptions" :center="selectedCountry.lat_long" :minZoom="1.4">
+  <l-map style="height: 500px; width: 100%" :options="mapOptions" :center="selectedCountry.lat_long" :zoom="zoom" :minZoom="1.4">
     <l-tile-layer :url="url"></l-tile-layer>
   </l-map>
 </template>
@@ -13,6 +13,7 @@ export default {
     return {
       url: `https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}.png?access_token=pk.eyJ1Ijoic2hhdW5oayIsImEiOiJjanhqYTEyazIxeTE4M3lzODMzYjdtNTdhIn0.zjHGZ4T6dhbdg5dQayUugQ`,
       selectedCountry: null,
+      zoom: 1.4,
       mapOptions: {
        zoomSnap: 0.1
      }
