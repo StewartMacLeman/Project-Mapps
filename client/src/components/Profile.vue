d6206906f0052ed622268d7b91dfe67c8432ca5d
psulli10 <petersullivan1985@gmail.com>
2019-07-01 21:04:06 +0100

    Profile.vue updated.

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,10 +1,18 @@
 <template lang="html">
 <div>
-<div v-if="showOne" class="factOne">{{selectedCountry.easy[0].correct_answer}}</div>
-  <div v-if="showTwo" class="factTwo"></div>
-  <div v-if="showThree" class="factThree"></div>
-  <div v-if="showFour" class="factFour"></div>
-  <div v-if="showFive" class="factFive"></div>
+<div v-if="showOne" class="factOne"><img :src="selectedCountry.easy[0].correct_answer" alt="image of Frenchg tricolour" height="200px" width="250px"></div>
+  <div v-if="showTwo" class="factTwo">
+    <p>Capital: {{selectedCountry.easy[1].correct_answer}}</p>
+  </div>
+  <div v-if="showThree" class="factThree">
+    <p>Language: {{selectedCountry.easy[2].correct_answer}}</p>
+  </div>
+  <div v-if="showFour" class="factFour">
+    <p>Landmark: {{selectedCountry.easy[3].correct_answer}}</p>
+  </div>
+  <div v-if="showFive" class="factFive">
+    <p>Food: {{selectedCountry.easy[4].correct_answer}}</p>
+  </div>
 </div>
 
 
@@ -18,10 +26,10 @@ export default {
   data(){
     return{
       showOne: true,
-      showTwo: false,
-      showThree: false,
-      showFour: false,
-      showFive: false
+      showTwo: true,
+      showThree: true,
+      showFour: true,
+      showFive: true
     }
   }
 
