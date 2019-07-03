9c2362663b57a03be7e84bd2956c58543575dd55
shaunhk <shaunhk@hotmail.com>
2019-07-03 11:21:47 +0100

    quotations added

--- client/src/components/QuizQuestions.vue
+++ client/src/components/QuizQuestions.vue
@@ -9,19 +9,19 @@
 
 
     <div v-if="showQuiz" class="questions">
-    <p>"{{selectedCountryQuestions[0].question}}"</p>
+    <p>"{{selectedCountryQuestions[0].question}}"gl</p>
     <br>
     <button type="button" name="answer-button" v-model="answerGiven" v-on:click="handleClick(answer)" v-for="answer in selectedCountryQuestions[0].answers">{{answer}}</button>
     </div>
 
     <div v-if="showResult" class="quiz-result" >
-      <h1>{{username}}... You scored {{correctAnswers}} out of 5!</h1>
-      <p v-if="correctAnswers === 0">Unlucky {{username}}. But you've learnt a lot today. Let's try again?</p>
-      <p v-if="correctAnswers === 1">Well, that's a start {{username}}! I wonder if you get better second time around...</p>
-      <p v-if="correctAnswers === 2">That's a decent score {{username}}! I bet next time you can do even better...</p>
-      <p v-if="correctAnswers === 3">Not bad at all {{username}}! Maybe one day you'll beat me.</p>
-      <p v-if="correctAnswers === 4">Woah! That's pretty good {{username}}. You've got me sweating here... I don't think you should play anymore.</p>
-      <p v-if="correctAnswers === 5">You beat me! Blast you and your smarts, {{username}}!</p>
+      <h1>{{username}}"... You scored {{correctAnswers}} out of 5!"</h1>
+      <p v-if="correctAnswers === 0">"Unlucky {{username}}. But you've learnt a lot today. Let's try again?"</p>
+      <p v-if="correctAnswers === 1">"Well, that's a start {{username}}! I wonder if you get better second time around..."</p>
+      <p v-if="correctAnswers === 2">"That's a decent score {{username}}! I bet next time you can do even better..."</p>
+      <p v-if="correctAnswers === 3">"Not bad at all {{username}}! Maybe one day you'll beat me."</p>
+      <p v-if="correctAnswers === 4">"Woah! That's pretty good {{username}}. You've got me sweating here... I don't think you should play anymore."</p>
+      <p v-if="correctAnswers === 5">"You beat me! Blast you and your smarts, {{username}}!"</p>
     </div>
 
     <div v-if="showResult" class="navigation-buttons">
